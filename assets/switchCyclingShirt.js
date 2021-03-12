a82b610a2d8b773e8cd5fa60f3b7c095d25244c3
Yann Raulet <yann.raulet@hotmail.fr>
2021-03-12 17:30:43 +0100

    class names modifications

--- assets/switchCyclingShirt.js
+++ assets/switchCyclingShirt.js
@@ -1,4 +1,4 @@
-const imgs = document.querySelectorAll('.img-select a');
+const imgs = document.querySelectorAll('.picture-select a');
 const imgBtns = [...imgs];
 let imgId = 1;
 
@@ -14,13 +14,13 @@ function slideImage(){
     if("matchMedia" in window) { // Detection
         if(window.matchMedia("(max-width:1023px)").matches) {
             //select the picture concerned
-            const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+            const displayWidth = document.querySelector('.picture-showcase img:first-child').clientWidth;
             //move the pictures in the X position, by choosing the width of the images as a reference
-            document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+            document.querySelector('.picture-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
         } else {
-            const displayHeight = document.querySelector('.img-showcase img:first-child').clientHeight;
+            const displayHeight = document.querySelector('.picture-showcase img:first-child').clientHeight;
             //move the pictures in the Y position, by choosing the height of the images as a reference
-            document.querySelector('.img-showcase').style.transform = `translateY(${- (imgId -1) * displayHeight}px)`;
+            document.querySelector('.picture-showcase').style.transform = `translateY(${- (imgId -1) * displayHeight}px)`;
         }
     }
 }
