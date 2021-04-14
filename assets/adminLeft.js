d02760a25cef3277bd9ee50ac3bb0ac7a86bc884
Yann Raulet <yann.raulet@hotmail.fr>
2021-04-14 09:55:23 +0200

    add admin left dashboard

---
+++ assets/adminLeft.js
@@ -0,0 +1,11 @@
+// Function to show or hide the left admin dashboard
+window.onload = function() {
+	init();
+};
+// Add this function, so the transition would only start running when the page has been fully loaded.
+// This is a fallback in case when someone has turned off their JavaScript. 
+
+function init() {
+	var menu = document.getElementById("menu");
+	menu.classList.add("transition-after-pageload");
+}
