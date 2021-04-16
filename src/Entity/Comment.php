642b1a7f6c4ba56ab299ac51f95b7671a05f13dc
FrKev1 <francisco.kevin37@gmail.com>
2021-04-16 16:36:06 +0200

    change blog comments

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -25,7 +25,7 @@ class Comment
     /**
      * @ORM\Column(type="datetime")
      */
-    private $createdAt;
+    private \DateTimeInterface $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Article::class, inversedBy="comments")
@@ -56,7 +56,7 @@ class Comment
         return $this;
     }
 
-   public function getCreatedAt(): ?\DateTimeInterface
+    public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
