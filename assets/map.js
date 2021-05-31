9941a54adc59ee3436677f5f61a8ab8112d84567
Yann Raulet <yann.raulet@hotmail.fr>
2021-05-31 10:26:42 +0200

    change leaflet map into mapbox

--- assets/map.js
+++ assets/map.js
@@ -9,13 +9,15 @@ let iconPicture = L.icon ({
  function initMap() {
     var map = L.map('mapId').setView([48.833, 2.333], 10); 
 
-    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         minZoom: 2,
         maxZoom: 13,
-        attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-
-    map.addLayer(osmLayer);
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoieWFubnJhdWxldCIsImEiOiJja3BjYzV0bTQwMTBqMzBvOGR1ZTBweDcxIn0.5jvTEj-WaEiKxIpSFpEu1Q'
+    }).addTo(map);
 
     //Checkbox for filters by years
     var command = L.control();
