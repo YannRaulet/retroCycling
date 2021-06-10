29abf0804436a230dc87dbbcd4b30ece5b8e75a6
Yann Raulet <yann.raulet@hotmail.fr>
2021-06-10 17:29:36 +0200

    modify scroll to top button

--- assets/app.js
+++ assets/app.js
@@ -27,56 +27,63 @@ require('bootstrap');
 
 // ---------------------------------------------------------------------------------
 // Functions Scroll to  the top
-//Get the button
 var mybutton = document.getElementById("btnScroll");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+let intervalId = 0; // Needed to cancel the scrolling when we're at the top of the page
+const $scrollButton = document.getElementById("btnScroll");
+
+function scrollStep() {
+  // Check if we're at the top already. If so, stop scrolling by clearing the interval
+  if (window.pageYOffset === 0) {
+    clearInterval(intervalId);
+  }
+  //returns the number of pixels the document scrolls
+  window.scroll(0, window.pageYOffset - 50);  
 }
 
-btnScroll.addEventListener("click", topFunction);
+function scrollToTop() {
+  // Call the function scrollStep() every 8 millisecons
+  intervalId = setInterval(scrollStep, 8);
+}
+$scrollButton.addEventListener('click', scrollToTop);
 
 // ---------------------------------------------------------------------------------
 // Lazy loading pictures
 document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy");    
-    var lazyloadThrottleTimeout;
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
-      
-      lazyloadThrottleTimeout = setTimeout(function() {
-          var scrollTop = window.pageYOffset;
-          lazyloadImages.forEach(function(img) {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-              }
-          });
-          if(lazyloadImages.length == 0) { 
-            document.removeEventListener("scroll", lazyload);
-            window.removeEventListener("resize", lazyload);
-            window.removeEventListener("orientationChange", lazyload);
-          }
-      }, 20);
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
     }
-    
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  });
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
